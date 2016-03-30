1e0bcd9755669a4ad844b687407b0f5a7eed14c6
rafabispo93 <rafabispo93@gmail.com>
2016-03-29 21:57:55 -0300

    Refatorado código models

--- BetIfba/bolao/models.py
+++ BetIfba/bolao/models.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    login = models.CharField(max_length=50,primary_key=True)
+    name = models.CharField(max_length=200)
+    password = models.CharField(min_lenght=5,max_length=15)
+    adm = models.BooleanField(default=false)
--- BetIfba/bolao/models/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.db import models
-
-class Users(models.Model):
-    login = models.CharField(max_length=50,primary_key=True)
-    name = models.CharField(max_length=200)
-    password = models.CharField(min_lenght=5,max_length=15)
-    adm = models.BooleanField(default=false)
-
